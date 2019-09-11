8887186c365d960855bc6073773378e04ab17fcd
Anthony Rawlins <anthony.rawlins@unimelb.edu.au>
2019-09-11 11:01:37 +1000

    Dockerizing the Express build

--- src/app/administration/import-config-section/import-config-section.module.ts
+++ src/app/administration/import-config-section/import-config-section.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ImportConfigCollectionComponent } from './import-config-collection/import-config-collection.component';
+import { SuiModule } from 'ng2-semantic-ui';
 
 
 @NgModule({
   declarations: [ImportConfigCollectionComponent],
   imports: [
     CommonModule,
+    SuiModule,
     FormsModule
   ],
   exports: [
